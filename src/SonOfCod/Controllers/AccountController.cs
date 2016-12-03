266f60c2a2e373d08a7145f358150dc0cc7c71e8
vp <notejde@gmail.com>
2016-12-03 10:14:14 -0800

    add UpdateAccountController

--- src/SonOfCod/Controllers/AccountController.cs
+++ src/SonOfCod/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SonOfCod.Models;
 using Microsoft.AspNetCore.Identity;
+using SonOfCod.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,6 +29,21 @@ namespace SonOfCod.Controllers
         {
             return View();
         }
-        // next add account/index
+        // next add view/account/index
+
+        //create register view
+        public async Task<IActionResult> Register (RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
