b554dfa8f669a075a866f2958fd1f19d0ac1dd3d
vp <notejde@gmail.com>
2016-12-05 13:27:44 -0800

    add SubscriberTest

---
+++ SonOfCod.Tests/SubscriberTest.cs
@@ -0,0 +1,23 @@
+﻿using SonOfCod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SonOfCod.Tests
+{
+    public class SubscriberTest
+    {
+        [Fact]
+        public void GetEmailTest()
+        {
+            //Arrange
+            var subscriber = new Subscriber();
+            //Act
+            var result = subscriber.Email;
+            //Assert
+            Assert.Equal("Talay@me", result);
+        }
+    }
+}
